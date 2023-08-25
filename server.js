4d93ec098b21e64bf3533474ffdbe24398754e15
AntoniaGranit <antonia.granit@gmail.com>
2023-08-25 21:16:26 +0300

    set up mongoose and install dependencies

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-plantmama";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+mongoose.set('debug', true);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -13,7 +20,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
 
 // Start the server
