48a45549689ae34259fe39db299eb5cd0c2fc217
AntoniaGranit <antonia.granit@gmail.com>
2023-09-02 13:56:20 +0300

    clean up route and comment code

--- routes/cart/removeitem.js
+++ routes/cart/removeitem.js
@@ -2,14 +2,12 @@ import express from 'express';
 const router = express.Router();
 import { Cart } from '../../schemas/cart';
 import { User } from '../../schemas/user';
-import { Plant } from '../../schemas/plant';
 import { authenticateUser } from '../../middleware/auth';
 
 router.delete('/cart/remove/:id', authenticateUser, async (req, res) => {
     try {
       const accessToken = req.header("Authorization");
       const singleUser = await User.findOne({accessToken: accessToken}); // Get the user ID connected to the cart
-      // const plantId = await Plant.findById(req.params.id); Get ID of plant user wants to add to cart
 
       // Find the user's cart
       let cart = await Cart.findOne({ owner: singleUser });
@@ -19,20 +17,19 @@ router.delete('/cart/remove/:id', authenticateUser, async (req, res) => {
         return res.status(404).json({ message: 'Cart not found' });
       }
   
-      // Find the index of the item in the cart
+      // Find the "first occurence" of an item in the cart with matching ID to the one user wants to remove
       const itemIndex = cart.items.findIndex(item => item.itemId.equals(req.params.id));
   
-      // If the item is in the cart, decrease its quantity or remove it
-      if (itemIndex !== -1) {
-        const item = cart.items[itemIndex];
-        if (item.quantity > 1) {
-          item.quantity -= 1;
-        } else {
-          cart.items.splice(itemIndex, 1);
+      if (itemIndex > -1) { // if the index of the item is 0 or greater, meaning it exists in the cart array
+        const item = cart.items[itemIndex]; // get the item from the cart array
+        if (item.quantity > 1) { // if the quantity is greater than 1
+          item.quantity -= 1; // decreate the quantity of this item by 1
+        } else { // otherwise
+          cart.items.splice(itemIndex, 1); // remove the item from the cart array entirely
         }
-        await cart.save();
-        await Cart.populate(cart, { path: 'items.itemId' });
-        res.json(cart);
+        await cart.save(); // save the updated cart to the database
+        await Cart.populate(cart, { path: 'items.itemId' }); // include detailed plant information in the response
+        res.json(cart); // the response with the updated cart
       } else {
         res.status(404).json({ message: 'Item not found in cart' });
       }
